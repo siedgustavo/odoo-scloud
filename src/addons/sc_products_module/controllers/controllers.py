50f0bace36eb9d2d94ed71d4bf107580e497fe92
siedgustavo <siedgustavo@gmail.com>
2021-11-22 18:20:38 -0500

    working in controllers

--- src/addons/sc_products_module/controllers/controllers.py
+++ src/addons/sc_products_module/controllers/controllers.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
  class sc_product_controller(http.Controller):
-     @http.route('/sc_products_module/', auth='user')
+     @http.route('/sc_products_module/', auth='public', website=True)
      def index(self, **kw):
-         return "SiedCloud - Products"
+         return "SiedCloud - Products - " + request.params["testParam"]
 
      @http.route('/sc_products_module/products/', auth='user')
      def list(self, **kw):
-         return http.request.render('my_module.listing', {
-             'root': '/sc_products_module/my_module',
-             'objects': http.request.env['sc_products_module.my_module'].search([]),
+         return http.request.render('sc_products_module.listing', {
+             'root': '/sc_products_module/products',
+             'objects': http.request.env['sc_products_module.sc_product'].search([]),
          })
 
-     @http.route('/sc_products_module/objects/<model("my_module.my_module"):obj>/', auth='public')
-     def object(self, obj, **kw):
-         return http.request.render('my_module.object', {
-             'object': obj
+     @http.route('/sc_products_module/products/<model("sc_products_module.sc_product"):obj>/', auth='user')
+     def object(self, product, **kw):
+         return http.request.render('sc_products_module.sc_product', {
+             'product': product
          })
