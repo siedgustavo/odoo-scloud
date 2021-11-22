498775fa32dc33ca0d8697e616ec4d2b5441dacc
siedgustavo <siedgustavo@gmail.com>
2021-11-22 17:21:24 -0500

    fix views

--- src/addons/sc_products_module/models/models.py
+++ src/addons/sc_products_module/models/models.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, http
+from odoo.exceptions import ValidationError
+from odoo.http import request
 
  class sc_product(models.Model):
-     _name = 'sc_product.product'
+     _name = 'sc_product'
 
-     name = fields.Char()
-     description = fields.Text()
-     product_category_id = fields.many2one('sc_product_category.category')
-     price = fields.Integer()
+     name = fields.Char('Nombre')
+     description = fields.Text('Descripcion')
+     product_category_id = fields.many2one('sc_product_category', 'Categoria')
+     price = fields.Float('Precio')
+     state = fields.Selection([('activo','Activo'),('desactivado','Desactivado')])
 
 
 class sc_product_category(models.Model):
-    _name = 'sc_product_category.category'
+    _name = 'sc_product_category'
 
-    name = fields.Char()
-    product_type_id = fields.many2one('sc_product_type.type')
-    description = fields.Text()
+    name = fields.Char('Nombre')
+    product_type_id = fields.many2one('sc_product_type', 'Tipo')
+    description = fields.Text('Descripcion')
+    state = fields.Selection([('activo', 'Activo'), ('desactivado', 'Desactivado')])
+
 
 class sc_product_type(models.Model):
-    _name = 'sc_product_type.type'
+    _name = 'sc_product_type'
 
-    name = fields.Char()
-    description = fields.Text()
+    name = fields.Char('Nombre')
+    description = fields.Text('Descripcion')
+    state = fields.Selection([('activo', 'Activo'), ('desactivado', 'Desactivado')])
